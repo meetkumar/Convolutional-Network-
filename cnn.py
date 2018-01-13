504b1075733fdc3d02cad4899b56675e972e846f
meetkumar <meetkumarpate.1504@gmail.com>
2018-01-13 11:22:15 -0800

    Added convolutional layer to increase the accuracy of network

--- cnn.py
+++ cnn.py
@@ -11,7 +11,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
